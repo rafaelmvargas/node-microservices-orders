a64d63f14dde66d1d4fb5225b1549d003802e653
Rafael Vargas <rafael.vargas@outlook.com>
2022-04-15 11:28:25 -0400

    add mongoose connect to database

--- orders.js
+++ orders.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+app.use(bodyParser.json());
 
+mongoose.connect('mongodb://localhost/orders', () => {
+  console.log('Datebase connected - Orders');
+});
 
 app.listen(7777, () => {
-    console.log('Up and running - Orders service')
-})
+  console.log('Up and running - Orders service');
+});
