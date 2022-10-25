9ce5bf7a6887093a15edbc16068025a10b55cfa4
Rafael Vargas <rafael.vargas@outlook.com>
2022-10-24 22:26:02 -0400

    create order

--- orders.js
+++ orders.js
@@ -9,6 +9,22 @@ mongoose.connect('mongodb://localhost/orders', () => {
   console.log('Datebase connected - Orders');
 });
 
+// Model is loaded
+require('./Order');
+const Order = mongoose.model('Order');
+
+// Will create a new order
+app.post('/order', (req, res) => {
+  var newOrder = new {
+    CustomerID: req.body.CustomerID,
+    BookID: req.body.BookID,
+    intialDate: req.body.intialDate,
+    deliveryDate: req.body.deliveryDate,
+  }();
+
+  var order = new Order(newOrder);
+});
+
 app.listen(7777, () => {
   console.log('Up and running - Orders service');
 });
